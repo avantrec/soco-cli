7473d1f6dca0265e7f846ca891de9dd8d1c9e437
Peter Toft <pwtoft@gmail.com>
2020-06-20 11:42:27 +0100

    Checkpoint

--- speaker-discovery.py
+++ speaker-discovery.py
@@ -1,25 +1,54 @@
+#!/usr/bin/env python3
+
 # Find Sonos households and speakers
 
 import ipaddress
 import socket
 import soco
+import ifaddr
 
 households = set()
 speakers = {}
 
-network = ipaddress.ip_network("192.168.0.0/24")
-for ip_addr in network:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.2)
-    endpoint = (str(ip_addr), 1400)
-    check = s.connect_ex(endpoint)
-    if check == 0:
-        speaker = soco.SoCo(str(ip_addr))
-        info = speaker.get_speaker_info()
-        print("Found", info["zone_name"], "at", str(ip_addr))
-        households.add(speaker.household_id)
-        if not speakers.get(info["zone_name"]):
-            speakers[info["zone_name"]] = (str(ip_addr), speaker.household_id)
+
+def is_ipv4_address(ip_address):
+    try:
+        ipaddress.IPv4Network(ip_address)
+        return True
+    except ValueError:
+        return False
+
+
+def find_my_ipv4_networks():
+    ip_list = []
+    adapters = ifaddr.get_adapters()
+    for adapter in adapters:
+        for ip in adapter.ips:
+            if is_ipv4_address(ip.ip):
+                # Omit the loopback address
+                if ip.ip != "127.0.0.1":
+                    ip_list.append(ip.ip + "/" + str(ip.network_prefix))
+    return ip_list
+
+
+# ToDo: make sure we select the group coordinator speaker
+ip_searched_list = []
+for ip_net in find_my_ipv4_networks():
+    network = ipaddress.ip_network(ip_net, False)
+    for ip_addr in network:
+        if not ip_addr in ip_searched_list:
+            ip_searched_list.append(ip_addr)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.2)
+            endpoint = (str(ip_addr), 1400)
+            check = s.connect_ex(endpoint)
+            if check == 0:
+                speaker = soco.SoCo(str(ip_addr))
+                info = speaker.get_speaker_info()
+                print("Found", info["zone_name"], "at", str(ip_addr))
+                households.add(speaker.household_id)
+                if not speakers.get(info["zone_name"]):
+                    speakers[info["zone_name"]] = (str(ip_addr), speaker.household_id)
 
 print(households)
 print()
