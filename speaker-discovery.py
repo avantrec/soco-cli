5c85b1e194a82b0fc034e6ae5ca21f24231ac3a8
pwt <1089749+pwt@users.noreply.github.com>
2020-06-20 17:14:47 +0100

    Checkpoint

--- speaker-discovery.py
+++ speaker-discovery.py
@@ -7,7 +7,6 @@ import socket
 import soco
 import ifaddr
 import threading
-import time
 
 households = set()
 speakers = {}
@@ -39,11 +38,12 @@ def find_my_ipv4_networks():
     return ipv4_net_list
 
 
-def check_ip(ip_list):
+# ToDo: make sure we select the coordinator speaker from speaker groups
+def check_ips(ip_list, timeout, sonos_devices):
     while len(ip_list) > 0:
         ip_addr = ip_list.pop(0)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
+        s.settimeout(timeout)
         if s.connect_ex((str(ip_addr), 1400)) == 0:
             speaker = soco.SoCo(str(ip_addr))
             info = speaker.get_speaker_info()
@@ -53,32 +53,35 @@ def check_ip(ip_list):
                 speakers[info["zone_name"]] = (str(ip_addr), speaker.household_id)
 
 
-# ToDo: make sure we select the coordinator speaker from speaker groups
-def search_ips():
+DEFAULT_THREADCOUNT = 128
+DEFAULT_TIMEOUT = 2  # seconds
+
+
+def scan_for_sonos(network=None, threadcount=DEFAULT_THREADCOUNT, timeout=DEFAULT_TIMEOUT):
+    """Returns a list of ..."""
+    ip_list = []
+    sonos_devices = []
+    # Set up the list of IPs to search
     for network in find_my_ipv4_networks():
         for ip_addr in network:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(1)
-            if s.connect_ex((str(ip_addr), 1400)) == 0:
-                speaker = soco.SoCo(str(ip_addr))
-                info = speaker.get_speaker_info()
-                print("Found", info["zone_name"], "at", str(ip_addr))
-                households.add(speaker.household_id)
-                if not speakers.get(info["zone_name"]):
-                    speakers[info["zone_name"]] = (str(ip_addr), speaker.household_id)
-
-    print(households)
-    print()
-    print(speakers)
+            ip_list.append(ip_addr)
+    # Start threads to check IPs for Sonos devices
+    threads = []
+    for _ in range(threadcount):
+        thread = threading.Thread(
+            target=check_ips, args=(ip_list, timeout, sonos_devices)
+        )
+        threads.append(thread)
+        thread.start()
+    # Wait for all threads to finish before returning
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
     # Populate IP list to search
-    ip_list = []
-    for network in find_my_ipv4_networks():
-        for ip_addr in network:
-            ip_list.append(ip_addr)
-    # Start threads
-    for _ in range(128):
-        thread = threading.Thread(target=check_ip, args=(ip_list,))
-        thread.start()
+    scan_for_sonos()
+
+    print(households)
+    print()
+    print(speakers)
