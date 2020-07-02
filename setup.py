18c1b33e8bd8d69fdd50dbe60d6d49f34b6e4505
pwt <1089749+pwt@users.noreply.github.com>
2020-07-02 11:27:46 +0100

    Checkpoint

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/avantrec/sonos-cli",
-    packages=setuptools.find_packages("sonos-cli"),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License",
