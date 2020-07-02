1eea763bc5114dc4128ae3e572d78764f25839d2
pwt <1089749+pwt@users.noreply.github.com>
2020-07-02 11:58:30 +0100

    Checkpoint

--- setup.py
+++ setup.py
@@ -3,20 +3,24 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+REQUIREMENTS = list(open("requirements.txt"))
+
 setuptools.setup(
-    name="sonos-cli",
+    name="soco-cli",
     version="0.0.1",
     author="Avantrec Ltd",
-    author_email="sonos-cli@avantrec.com",
+    author_email="soco-cli@avantrec.com",
     description="Sonos command line utility, based on SoCo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/avantrec/sonos-cli",
+    url="https://github.com/avantrec/soco-cli",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
+    install_requires=REQUIREMENTS,
+    entry_points={"console_scripts": ["sonos = soco_cli.sonos:main"]},
 )
