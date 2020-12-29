112ce6ba23451ec8f961259635bf7df1765610a2
pwt <1089749+pwt@users.noreply.github.com>
2020-12-29 16:13:21 +0000

    'play_file' tidy ups

--- soco_cli/play_local_file.py
+++ soco_cli/play_local_file.py
@@ -44,18 +44,18 @@ def http_server(server_ip, directory, filename):
     MyHTTPHandler.filename = filename
 
     # Try ports within the range, in sequence
-    httpd = None
     for port in range(PORT_START, PORT_END + 1):
         try:
             httpd = HTTPServer((server_ip, port), handler)
-            break
+            logging.info("Using {}:{} for web server".format(server_ip, port))
+            httpd_thread = Thread(target=httpd.serve_forever, daemon=True)
+            httpd_thread.start()
+            logging.info("Web server started")
+            return httpd
         except OSError:
             continue
-    logging.info("Using {}:{} for web server".format(server_ip, port))
-    httpd_thread = Thread(target=httpd.serve_forever, daemon=True)
-    httpd_thread.start()
-    logging.info("Web server started")
-    return httpd
+
+    return None
 
 
 def get_server_ip(speaker):
@@ -73,49 +73,6 @@ def get_server_ip(speaker):
     return None
 
 
-def play_local_file(speaker, pathname):
-    # speaker is a SoCo instance
-    # pathname is the local file to be played
-
-    if not path.exists(pathname):
-        error_and_exit("File '{}' not found".format(pathname))
-        return False
-    directory, filename = path.split(pathname)
-
-    # Sanitise filename for URL
-    filename = urllib.parse.quote(filename)
-
-    server_ip = get_server_ip(speaker)
-    if not server_ip:
-        logging.info("No suitable server IP found")
-        error_and_exit("Can't determine the server_ip IP address")
-        return False
-    logging.info("Using server IP address: {}".format(server_ip))
-
-    # Start the webserver (runs in a daemon thread)
-    httpd = http_server(server_ip, directory, filename)
-    if not httpd:
-        exit(1)
-
-    uri = "http://" + server_ip + ":" + str(httpd.server_port) + "/" + filename
-    logging.info("Playing file '{}' from directory '{}'".format(filename, directory))
-    speaker.play_uri(uri)
-
-    # Flag that playback is stopped in the event of a CTRL-C
-    logging.info("Setting flag to stop playback on signal")
-    set_speaker_playing_local_file(speaker)
-
-    # Wait until the track has finished, then shut down the
-    # web server
-    logging.info("Waiting for playback to stop")
-    wait_until_stopped(speaker)
-    logging.info("Playback stopped")
-    httpd.shutdown()
-    logging.info("Web server thread terminated")
-    set_speaker_playing_local_file(None)
-    return True
-
-
 def wait_until_stopped(speaker):
     sub = speaker.avTransport.subscribe(auto_renew=True)
     while True:
@@ -131,3 +88,47 @@ def wait_until_stopped(speaker):
                 return True
         except Empty:
             pass
+
+
+def play_local_file(speaker, pathname):
+    # speaker is a SoCo instance
+    # pathname is the local file to be played
+
+    if not path.exists(pathname):
+        error_and_exit("File '{}' not found".format(pathname))
+        return False
+    directory, filename = path.split(pathname)
+
+    # Sanitise filename for URL
+    url_filename = urllib.parse.quote(filename)
+
+    server_ip = get_server_ip(speaker)
+    if not server_ip:
+        error_and_exit("Can't determine the server_ip IP address")
+        return False
+    logging.info("Using server IP address: {}".format(server_ip))
+
+    # Start the webserver (runs in a daemon thread)
+    httpd = http_server(server_ip, directory, url_filename)
+    if not httpd:
+        error_and_exit("Cannot create HTTP server")
+        return False
+
+    uri = "http://" + server_ip + ":" + str(httpd.server_port) + "/" + url_filename
+    logging.info("Playing file '{}' from directory '{}'".format(filename, directory))
+    logging.info("Playback URI: {}".format(uri))
+    speaker.play_uri(uri)
+
+    # Flag that playback is stopped in the event of a CTRL-C
+    logging.info("Setting flag to stop playback on signal")
+    set_speaker_playing_local_file(speaker)
+
+    # Wait until the track has finished, then shut down the
+    # web server
+    logging.info("Waiting for playback to stop")
+    wait_until_stopped(speaker)
+    logging.info("Playback stopped ... terminating web server")
+    httpd.shutdown()
+    logging.info("Web server thread terminated")
+    set_speaker_playing_local_file(None)
+    return True
