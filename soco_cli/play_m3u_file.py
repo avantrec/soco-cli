9a0b7ef89c173e41ec8ca144567475214d40bf7d
pwt <1089749+pwt@users.noreply.github.com>
2021-01-04 21:38:52 +0000

    Checkpoint: multiprocessing change

--- soco_cli/play_m3u_file.py
+++ soco_cli/play_m3u_file.py
@@ -1,11 +1,11 @@
 import logging
 import os
 from os import chdir, name, path
+from multiprocessing import Process
 from pathlib import Path
 from random import choice, sample
-import signal
+from soco import SoCo
 import sys
-from threading import Thread
 from .m3u_parser import parse_m3u
 from .play_local_file import play_local_file, is_supported_type
 from .utils import error_and_exit, set_sigterm
@@ -36,43 +36,51 @@ def wait_for_keypress():
     return result
 
 
-def interactive_thread(speaker):
-    set_sigterm(True)
-    keypress = wait_for_keypress()
-    if keypress in ["N", "n"]:
-        action = "NEXT"
-        print("Next track ...")
-        speaker.stop()
-        logging.info(
-            "Interactive mode: key = '{}', action = '{}'".format(keypress, action)
-        )
-    if keypress in ["P", "p"]:
-        action = "PAUSE"
-        print("Pause playback ...")
+def interaction_manager(speaker_ip):
+    sys.stdin = open(0)
+    speaker = SoCo(speaker_ip)
+    while True:
         try:
-            speaker.pause()
-        except Exception as e:
-            logging.info("Exception ignored: {}".format(e))
+            keypress = wait_for_keypress()
+        except:
+            keypress = None
             pass
-        logging.info(
-            "Interactive mode: key = '{}', action = '{}'".format(keypress, action)
-        )
-    if keypress in ["R", "r"]:
-        action = "RESUME"
-        print("Resume playback ...")
-        try:
-            speaker.play()
-        except Exception as e:
-            logging.info("Exception ignored: {}".format(e))
-            pass
-        logging.info(
-            "Interactive mode: key = '{}', action = '{}'".format(keypress, action)
-        )
-    # Windows captures CTRL-C key-presses, so we handle them directly here
-    if name == "nt" and keypress == "\x03":
-        logging.info("Windows CTRL-C: Stopping speaker '{}' and exiting".format(speaker.player_name))
-        speaker.stop()
-        os._exit(0)
+        if keypress in ["N", "n"]:
+            action = "NEXT"
+            print("Next track ...")
+            speaker.stop()
+            logging.info(
+                "Interactive mode: key = '{}', action = '{}'".format(keypress, action)
+            )
+        if keypress in ["P", "p"]:
+            action = "PAUSE"
+            print("Pause playback ...")
+            try:
+                speaker.pause()
+            except Exception as e:
+                logging.info("Exception ignored: {}".format(e))
+                pass
+            logging.info(
+                "Interactive mode: key = '{}', action = '{}'".format(keypress, action)
+            )
+        if keypress in ["R", "r"]:
+            action = "RESUME"
+            print("Resume playback ...")
+            try:
+                speaker.play()
+            except Exception as e:
+                logging.info("Exception ignored: {}".format(e))
+                pass
+            logging.info(
+                "Interactive mode: key = '{}', action = '{}'".format(keypress, action)
+            )
+        # Windows captures CTRL-C key-presses, so we handle them directly here
+        if name == "nt" and keypress == "\x03":
+            logging.info("Windows CTRL-C: Stopping speaker '{}' and exiting".format(speaker.player_name))
+            speaker.stop()
+            os._exit(0)
+
+    # sys.stdin.close()
 
 
 def play_m3u_file(speaker, m3u_file, options=""):
@@ -117,8 +125,21 @@ def play_m3u_file(speaker, m3u_file, options=""):
     if directory != "":
         chdir(directory)
 
+    # Interactive mode
+    keypress_process = None
     if "i" in options:
         print("Interactive mode actions: (N)ext, (P)ause, (R)esume, CTRL-C")
+        try:
+            logging.info("Interactive mode ... starting keypress process")
+            keypress_process = Process(
+                target=interaction_manager, args=(speaker.ip_address,), daemon=True
+            )
+            keypress_process.start()
+            logging.info("Process PID {} created".format(keypress_process.pid))
+        except Exception as e:
+            logging.info("Exception ignored: {}".format(e))
+            keypress_process = None
+            pass
 
     zero_pad = len(str(len(tracks)))
     for index, track in enumerate(tracks):
@@ -133,19 +154,6 @@ def play_m3u_file(speaker, m3u_file, options=""):
             print("Error: unsupported file type:", abs_filename)
             continue
 
-        # Interactive mode
-        keypress_thread = None
-        if "i" in options:
-            try:
-                logging.info("Interactive mode ... starting keypress thread")
-                keypress_thread = Thread(
-                    target=interactive_thread, args=(speaker,), daemon=True
-                )
-                keypress_thread.start()
-            except Exception as e:
-                logging.info("Exception ignored: {}".format(e))
-                pass
-
         if "p" in options:
             print(
                 "Playing {} of {}:".format(str(index + 1).zfill(zero_pad), len(tracks)),
@@ -154,7 +162,7 @@ def play_m3u_file(speaker, m3u_file, options=""):
 
         play_local_file(speaker, abs_filename)
 
-        if keypress_thread:
-            keypress_thread.join(timeout=0.1)
+    if keypress_process:
+        keypress_process.terminate()
 
     return True
