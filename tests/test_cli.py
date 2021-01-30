fa39cf2bc9b4d1b08699e99b5f43d8fcc9a21e39
pwt <1089749+pwt@users.noreply.github.com>
2021-01-30 16:18:23 +0000

    Add API test stub

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,7 @@ import unittest
 import soco
 
 from soco_cli import action_processor as ap
+from soco_cli.api import run_command
 
 speaker_1 = soco.SoCo("192.168.0.42")
 speaker_2 = soco.SoCo("192.168.0.39")
@@ -28,6 +29,14 @@ def test_cli(capsys):
         ap.process_action(test[0], test[1], test[2], use_local_speaker_list=True)
         out, err = capsys.readouterr()
         assert out == test[3]
+
+
+def test_api():
+    for test in tests:
+        exit_code, output, error_msg = run_command(
+            test[0], test[1], *test[2], use_local_speaker_list=True
+        )
+        assert output == test[3].rstrip()
 
 
 # class TestVolEQ(unittest.TestCase):
