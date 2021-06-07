384a87643858bd553256cf8febf4424ab16436db
pwt <1089749+pwt@users.noreply.github.com>
2021-06-07 12:09:22 +0100

    Use unbuffered print() in 'track_follow'

--- soco_cli/track_follow.py
+++ soco_cli/track_follow.py
@@ -18,7 +18,7 @@ def track_follow(speaker, use_local_speaker_list=False, break_on_pause=True):
     'run_command()' API call is used.
     """
 
-    print()
+    print(flush=True)
     while True:
         # If stopped, wait for the speaker to start playback
         _, state, _ = run_command(
@@ -28,7 +28,7 @@ def track_follow(speaker, use_local_speaker_list=False, break_on_pause=True):
             "STOPPED",
             "PAUSED_PLAYBACK",
         ]:
-            print("  Playback is stopped or paused\n")
+            print("  Playback is stopped or paused\n", flush=True)
             if break_on_pause:
                 logging.info("Playback is paused/stopped; returning")
                 break
@@ -54,9 +54,9 @@ def track_follow(speaker, use_local_speaker_list=False, break_on_pause=True):
             output = output.replace("Artist:", "Artist:     ")
             output = output.replace("Duration:", "Duration:   ")
             output = output.replace("Title:", "Title:      ")
-            print(output)
+            print(output, flush=True)
         else:
-            print(error_msg)
+            print(error_msg, flush=True)
 
         # Wait until the track changes
         logging.info("Waiting for end of track")
