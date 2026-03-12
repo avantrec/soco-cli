555d8a508065c2b675da1a8a8628fa3a457abd12
pwt <1089749+pwt@users.noreply.github.com>
2026-03-12 12:18:48 +0000

    Fix tab completion in interactive mode for Python 3.10+

--- soco_cli/interactive.py
+++ soco_cli/interactive.py
@@ -8,6 +8,11 @@ import sys
 try:
     import readline
 
+    if "libedit" in readline.__doc__:
+        readline.parse_and_bind("bind ^I rl_complete")
+    else:
+        readline.parse_and_bind("tab: complete")
+
     RL = True
     UNIX = True
     WINDOWS = False
